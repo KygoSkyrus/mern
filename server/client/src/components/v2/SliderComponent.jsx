a09bf89283257f6aa9cef99ba4930952035482cc
Dheeraj Gupta <dheerajg@digitizeco.in>
2023-08-03 16:18:00 +0530

    .

---
+++ server/client/src/components/v2/SliderComponent.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SliderComponent = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,//how many slides will be shown
+    slidesToScroll: 1,//at onescroll how many slide will be slid
+    initialSlide:3,//tells from where to start
+    fade: true, //slide fades on change
+    //lazyLoad: true,
+    //adaptiveHeight: true//for unequal heights
+    //autoplay: true,autoplaySpeed: 2000,cssEase: "linear",// to keep playing automatically
+    //pauseOnHover: true//works with autoplay and stops on hover
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  return (
+    <>
+      <div>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default SliderComponent;
