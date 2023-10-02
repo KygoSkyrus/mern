ef3bf59379a93de117e72c629647180e1f1a9c40
KygoSkyrus <guptadheeraj371@gmail.com>
2023-10-02 23:14:53 +0530

    .

--- server/client/src/components/v2/Admin/Nav.js
+++ server/client/src/components/v2/Admin/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { productFormVisibility, setProductFormTitle } from './../redux/todoSlice'
@@ -12,15 +12,64 @@ const Nav = () => {
   const visibility = useSelector(state => state.productFormVisibility.visibility)
 
 
+  useEffect(() => {
+    // getSidebarWorking()
+    handleSelectedOption()
+  }, [])
+
   const handleAddClick = () => {
     if (!visibility) {
       dispatch(clearProductForm());
       dispatch(productFormVisibility({ visibility: !visibility }));
-      dispatch(setProductFormTitle({title:"Add product"}))
+      dispatch(setProductFormTitle({ title: "Add product" }))
       console.log('ccc')
     }
   };
 
+
+  //   function getSidebarWorking() {
+  //      const btnToggler = document.querySelector(".btn-trapezoid-outline");
+  //     // const navbar = document.querySelector(".sidebar");
+  //     const menuItem = document.querySelectorAll(".menu-item");
+
+  //     btnToggler.addEventListener('click', () => {
+  //         navbar.classList.toggle('active');
+  //         menuItem?.forEach(x => { x.classList.toggle('pl'); x.classList.toggle('w') })
+  //     });
+  // }
+
+  function handleSelectedOption(e) {
+    let currentOption;
+
+    switch (window.location.href) {
+
+      case window.location.origin + "/admin/dashboard": common("dashboard")
+        break;
+
+      case window.location.origin + "/admin/users": common("users")
+        break;
+
+      case window.location.origin + "/admin/orders": common("orders")
+        break;
+
+      default:
+        break;
+    }
+
+    function common(data) {
+      document.querySelector(`[data-link="${data}"]`)?.classList.add('active')
+      currentOption = data
+    }
+
+    //removing selected class from other options
+    document.querySelector('.btn-trapezoid-outline')?.childNodes.forEach(x => {
+      if (x.classList.contains('active')) {
+        if (x.dataset.link !== currentOption) {
+          x.classList.remove('active')
+        }
+      }
+    })
+  }
 
 
   return (
@@ -28,23 +77,26 @@ const Nav = () => {
 
       <div className='d-flex align-items-center'>
 
-        <div className="btn-trapezoid-outline" >
-          <section className='nav-icons'>
+        <div className="btn-trapezoid-outline" onClick={e => handleSelectedOption(e)} >
+          <Link className='nav-icons text-white py-2' data-link="dashboard" to='/admin/dashboard'>
             <i className="fa-solid fa-shopping-bag" aria-hidden="true"></i>
-          </section>
+          </Link>
 
-          <Link className='nav-icons text-white' to='/admin/orders'>
+          <Link className='nav-icons text-white py-2 active' data-link="orders" to='/admin/orders'>
             <i className="fa-solid fa-table" aria-hidden="true"></i>
           </Link>
 
-          <p>{visibility}</p>
-          <section className='nav-icons' onClick={handleAddClick}>
+          <Link className='nav-icons text-white py-2' data-link="users" to='/admin/users'>
+            <i className="fa-regular fa-user" aria-hidden="true"></i>
+          </Link>
+
+          <section className='nav-icons py-2' onClick={handleAddClick}>
             <i className="fa-solid fa-add" aria-hidden="true"></i>
           </section>
 
         </div>
       </div>
-      
+
 
     </>
   )
