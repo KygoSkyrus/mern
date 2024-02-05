1e338c537ca2f08d5e776328749d39c0f067016f
KygoSkyrus <guptadheeraj371@gmail.com>
2024-02-05 23:18:32 +0530

    css changes

--- server/client/src/firebaseConfig.js
+++ server/client/src/firebaseConfig.js
@@ -1,18 +1,9 @@
-// export const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: "shopp-itt.firebaseapp.com",
-//     projectId: "shopp-itt",
-//     storageBucket: "shopp-itt.appspot.com",
-//     messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
-//     appId:process.env.REACT_APP_APP_ID,
-//     measurementId: process.env.REACT_APP_MEASUREMENT_ID
-//   }
 export const firebaseConfig = {
-  apiKey: "AIzaSyD356cys4X2N0DHboL4T8MZCDR1BuN2n88",
-  authDomain: "shopp-itt.firebaseapp.com",
-  projectId: "shopp-itt",
-  storageBucket: "shopp-itt.appspot.com",
-  messagingSenderId: "500784370915",
-  appId: "1:500784370915:web:5433a992ab3e3229daa1d6",
-  measurementId: "G-DVFRLB25DQ"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "shopp-itt.firebaseapp.com",
+    projectId: "shopp-itt",
+    storageBucket: "shopp-itt.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  }
